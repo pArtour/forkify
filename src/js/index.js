fed4e326378a1afb9c3ebaab5ab6920dcb99998f
pArtour <edvardi72@gmail.com>
2020-03-12 22:10:48 +0200

    beautifying the recipes title

--- src/js/index.js
+++ src/js/index.js
@@ -12,9 +12,6 @@ const state = {};
 const controlSearch = async () => {
   // 1) get query from view
   const query = searchView.getInput() // to do
-  console.log(query);
-  
-
   if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
@@ -23,10 +20,9 @@ const controlSearch = async () => {
     searchView.clearResults();
     // 4) Search for recipes
     await state.search.getResults();
-
     // 5) Render results on UI
     searchView.renderResults(state.search.result);
-  }
+  };
 };
 
 elements.searchForm.addEventListener('submit', event => {
