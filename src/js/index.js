717674eb5a71f0fbac4004146944794a1f697885
pArtour <edvardi72@gmail.com>
2020-03-12 21:55:10 +0200

    Displaying recipes on UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,35 @@
-import Search from './modules/Search'
+import Search from './modules/Search';
+import * as searchView from './views/search-view';
+import { elements } from './views/base';
+// Global state of the app
+// * - Search object
+// * - Current recipe object
+// * - Shopping list object
+// * - Liked recipes
 
-const search = new Search('pizza');
+const state = {};
 
-console.log(search);
-search.getResults();
+const controlSearch = async () => {
+  // 1) get query from view
+  const query = searchView.getInput() // to do
+  console.log(query);
+  
+
+  if (query) {
+    // 2) New search object and add to state
+    state.search = new Search(query);
+    // 3)  Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
+    // 4) Search for recipes
+    await state.search.getResults();
+
+    // 5) Render results on UI
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  controlSearch();
+});
