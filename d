69e5eef3f4ebd6d8b2e7d20e63cea743367fd396
pArtour <edvardi72@gmail.com>
2020-03-14 15:41:00 +0200

    Shopping list controller &  Likes model + likes view + likes localStorage

---
+++ d
@@ -0,0 +1,96 @@
+[33mcommit bcb86d46b8d7fb0d593f8f3362427e1ae8d8c3c6[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Sat Mar 14 09:30:54 2020 +0200
+
+    Creating the list
+
+[33mcommit 7d518ec65d8bf594027b41588d83f524fa4eb339[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Fri Mar 13 22:07:19 2020 +0200
+
+    Updating recipe servings
+
+[33mcommit 80b34d515b899d13803fc223486aff899f6fcac1[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Fri Mar 13 21:36:15 2020 +0200
+
+    Formating units
+
+[33mcommit 2bae6af391ed47c44add101a122652ca15d5d95a[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Fri Mar 13 21:09:55 2020 +0200
+
+    Rendering recipes
+
+[33mcommit 2effefca26f767d79ad0b1e286fa06817b6ad79a[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Fri Mar 13 10:39:28 2020 +0200
+
+    Formmating and parsing ingredients
+
+[33mcommit 395dd139bc8f15fc6ef8b7d1ff2429226cb0d34a[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Fri Mar 13 09:38:32 2020 +0200
+
+    Getting recipe on hashchange & load events
+
+[33mcommit a102193d5183c9018f59cb449b857e4fc58c12ec[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Fri Mar 13 09:04:11 2020 +0200
+
+    Pagination
+
+[33mcommit 66e067662e53c264597ce1ddbf23eccc879fe9c4[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Thu Mar 12 22:39:54 2020 +0200
+
+    Created loader
+
+[33mcommit fed4e326378a1afb9c3ebaab5ab6920dcb99998f[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Thu Mar 12 22:10:48 2020 +0200
+
+    beautifying the recipes title
+
+[33mcommit 717674eb5a71f0fbac4004146944794a1f697885[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Thu Mar 12 21:55:10 2020 +0200
+
+    Displaying recipes on UI
+
+[33mcommit 25edf8d932e986c06b618013fb84ccdca03c7c4c[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Thu Mar 12 10:35:28 2020 +0200
+
+    Search block (Search obj)
+
+[33mcommit 96318ebdea55d8f5e565ea1de1d6f93f8daf3d4f[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Wed Mar 11 11:20:38 2020 +0200
+
+    babel
+
+[33mcommit 7fabf5c956310096badb60c96d8638aa299149c9[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Wed Mar 11 09:07:14 2020 +0200
+
+    configs
+
+[33mcommit fa7f578e1f1504c9e1f6ae66ae0bc8baf7f61795[m
+Merge: 5d48841 d760a1c
+Author: pArtour <edvardi72@gmail.com>
+Date:   Tue Mar 10 20:16:27 2020 +0200
+
+    Merge branch 'master' of github.com:pArtour/forkify
+
+[33mcommit 5d488417a1fdd13f5033bff33dd5d4b6ebf1f28f[m
+Author: pArtour <edvardi72@gmail.com>
+Date:   Tue Mar 10 20:04:50 2020 +0200
+
+    First commit
+
+[33mcommit d760a1c7530d34716ca7cc45dec582a28a69d8b2[m
+Author: Arthur Pushkov <48758714+pArtour@users.noreply.github.com>
+Date:   Tue Mar 10 20:03:30 2020 +0200
+
+    Initial commit
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,11 @@
 import Search from './modules/Search';
 import Recipe from './modules/Recipe';
 import List from './modules/List';
+import Likes from './modules/Likes';
 import * as searchView from './views/search-view';
 import * as recipeView from './views/recipe-view';
+import * as listView from './views/list-view';
+import * as likeView from './views/likes-view';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 // Global state of the app
@@ -10,8 +13,10 @@ import { elements, renderLoader, clearLoader } from './views/base';
 // * - Current recipe object
 // * - Shopping list object
 // * - Liked recipes
+
 const state = {};
 
+window.state = state;
 // * Search controller
 const controlSearch = async () => {
   // 1) get query from view
@@ -77,7 +82,7 @@ const controlRecipe = async () => {
   
       // Render recipe
       clearLoader();
-      recipeView.renderRecipe(state.recipe);
+      recipeView.renderRecipe(state.recipe, state.likes.isLiked(id));
       
     } catch (error) {
       alert('Error processing recipe!');
@@ -86,6 +91,79 @@ const controlRecipe = async () => {
 };
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+// List controller
+const controlList = () => {
+  // Create a new list IF there si not yet
+  if (!state.list) state.list = new List();
+
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(elem => {
+    const item = state.list.addItem(elem.count, elem.unit, elem.ingredient);
+    listView.renderItem(item);
+  });
+};
+
+
+
+// handle delete and update list items events
+elements.shopping.addEventListener('click', event => {
+  const id = event.target.closest('.shopping__item').dataset.itemId;
+  // handle the delete event
+  if (event.target.matches('.shopping__delete, .shopping__delete *')) {
+    // Delete from stete
+    state.list.deleteItem(id);
+    // Delete from UI
+    listView.deleteItem(id);
+    // Handle the count update
+  } else if (event.target.matches('.shopping__count-value')) {
+    const val = parseFloat(event.target.value);
+    state.list.updateCount(id, val);
+  }
+});
+
+// Like controller
+const controlLike = () => {
+  if (!state.likes) state.likes = new Likes();
+  const currentId = state.recipe.id;
+  // User has not yet liked current recipe
+  if (!state.likes.isLiked(currentId)) {
+    // Add like to the state
+    const newLike = state.likes.addLike(
+      currentId,
+      state.recipe.title,
+      state.recipe.author,
+      state.recipe.img,
+    );
+    // Toggle the like btn
+      likeView.toggleLikeBtn(true)
+    // Add like to the UI list
+      likeView.renderLike(newLike);
+      console.log(state.likes);
+      
+  // User has liked current recipe
+  } else {
+     // Remove like from the state
+      state.likes.deleteLike(currentId)
+     // Toggle the like btn
+      likeView.toggleLikeBtn(false)
+     // Remove like from the UI list
+      console.log(state.likes);
+      likeView.deleteLike(currentId);
+  }
+  likeView.toggleLikeMenu(state.likes.getNumLikes())
+};
+
+// Restore like recipes on page load
+window.addEventListener('load', () => {
+  state.likes = new Likes();
+  // Restore likes
+  state.likes.readStorage();
+  // Toggle like btn
+  likeView.toggleLikeMenu(state.likes.getNumLikes());
+  // Render the existing likes
+  state.likes.likes.forEach(like => likeView.renderLike(like));
+})
 
 // handling recipe button clicks
 elements.recipe.addEventListener('click', event => {
@@ -98,13 +176,14 @@ elements.recipe.addEventListener('click', event => {
  } else if (event.target.matches('.btn-increase, .btn-increase *')) {
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+ } else if (event.target.matches('.recipe__btn-add, recipe__btn-add *')) {
+  //  Add ingredirnts and shopping list
+    controlList()
+ } else if(event.target.matches('.recipe__love, .recipe__love *')) {
+  //  Like controller
+    controlLike()
  }
- console.log(state.recipe);
 });
-
-const l = new List();
-
-window.l = l;
 
 
 
